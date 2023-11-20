913f1b31fd143eac0d210745b87b1390e597d3a5
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-11-20 20:12:48 +0200

    Edit ts-config

--- build/src/pages/Jobs/Jobs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-require("./Jobs.scss");
-const Jobs = () => {
-    return (0, jsx_runtime_1.jsx)("div", { className: 'jobs', children: "Jobs" });
-};
-exports.default = Jobs;
