97b5b057f600a213f7fff0f0eae2cd7c368fd8d3
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-11-30 07:43:27 +0200

    Edit

--- src/Views/SideBar/SideBar.tsx
+++ src/Views/SideBar/SideBar.tsx
@@ -1,5 +1,5 @@
-import FeedIdentity from './Helpers/FeedIdentity/FeedIdentity.tsx';
-import RecentIdentity from './Helpers/RecentIdentity/RecentIdentity.tsx';
+import FeedIdentity from './Helpers/FeedIdentity/FeedIdentity';
+import RecentIdentity from './Helpers/RecentIdentity/RecentIdentity';
 import './SideBar.scss';
 
 const SideBar = () => {
