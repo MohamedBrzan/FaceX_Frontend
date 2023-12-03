c578d51f60f24fa312393e9532ed2f301b052f15
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-03 09:17:24 +0200

    Redux Toolkit Connected Successfully

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.scss';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const element = document.getElementById('root');
 createRoot(element!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
 // ReactDOM;
