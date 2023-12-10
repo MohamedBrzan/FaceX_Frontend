0e20839d7ca87dd0e8752f88696bd3e1047c69c4
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-10 09:20:34 +0200

    Finished Sign Up carousel

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import FormGroup from 'react-bootstrap/FormGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import Footer from '../../Views/Footer/Footer';
@@ -9,12 +11,55 @@ import appleImg from '../../assets/apple.png';
 import googleImg from '../../assets/google_logo.jpeg';
 import facebookImg from '../../assets/facebook.png';
 import './Authentication.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const SignUp = () => {
   const [firstName, setFirstName] = useState<string>('');
   const [lastName, setLastName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const rightArrowRef = useRef<HTMLDivElement>(null);
+  const leftArrowRef = useRef<HTMLDivElement>(null);
+  const rowRef = useRef<HTMLDivElement>(null);
+  const singUpButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const left = leftArrowRef?.current;
+    const right = rightArrowRef?.current;
+    const row = rowRef?.current;
+    const submitButton = singUpButtonRef?.current;
+
+    if (firstName == '' || lastName == '' || email == '' || password == '') {
+      submitButton?.classList.add('hidden');
+    } else {
+      submitButton?.classList.remove('hidden');
+    }
+
+    left?.addEventListener('click', () => {
+      row?.classList.remove('move');
+      left.classList.add('disabled');
+      right?.classList.remove('disabled');
+    });
+
+    right?.addEventListener('click', () => {
+      row?.classList.add('move');
+      right.classList.add('disabled');
+      left?.classList.remove('disabled');
+    });
+
+    submitButton?.addEventListener('click', () => {
+      if (firstName == '' || lastName == '') {
+        return left?.click();
+      } else if (email == '' || password == '') {
+        return right?.click();
+      }
+    });
+  }, [email, firstName, lastName, password]);
+
   return (
     <section className='authentication sign_up'>
       <section className='form'>
@@ -23,60 +68,75 @@ const SignUp = () => {
           <small>Make the most of your professional life</small>
         </p>
         <Form>
-          <FormGroup className='mb-3'>
-            <Input
-              value={firstName}
-              type='text'
-              placeholder='First Name'
-              label='First Name'
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const targetVal = e.target as HTMLInputElement;
-                setFirstName(targetVal.value);
-              }}
-              required={true}
-            />
-          </FormGroup>
-          <FormGroup className='mb-3'>
-            <Input
-              value={lastName}
-              type='text'
-              placeholder='Last Name'
-              label='Last Name'
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const targetVal = e.target as HTMLInputElement;
-                setLastName(targetVal.value);
-              }}
-              required={true}
-            />
-          </FormGroup>
-          <FormGroup className='mb-3'>
-            <Input
-              value={email}
-              type='email'
-              placeholder='Email Address'
-              label='Email Address'
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const targetVal = e.target as HTMLInputElement;
-                setEmail(targetVal.value);
-              }}
-              required={true}
-            />
-          </FormGroup>
-          <FormGroup className='mb-3'>
-            <Input
-              value={password}
-              type='password'
-              placeholder='Password'
-              label='Password'
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const targetVal = e.target as HTMLInputElement;
-                setPassword(targetVal.value);
-              }}
-              required={true}
-            />
-          </FormGroup>
+          <Row ref={rowRef}>
+            <Col xs={12}>
+              <FormGroup className='mb-3'>
+                <Input
+                  value={firstName}
+                  type='text'
+                  placeholder='First Name'
+                  label='First Name'
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const targetVal = e.target as HTMLInputElement;
+                    setFirstName(targetVal.value);
+                  }}
+                  required={true}
+                />
+              </FormGroup>
+              <FormGroup className='mb-3'>
+                <Input
+                  value={lastName}
+                  type='text'
+                  placeholder='Last Name'
+                  label='Last Name'
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const targetVal = e.target as HTMLInputElement;
+                    setLastName(targetVal.value);
+                  }}
+                  required={true}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs={12}>
+              <FormGroup className='mb-3'>
+                <Input
+                  value={email}
+                  type='email'
+                  placeholder='Email Address'
+                  label='Email Address'
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const targetVal = e.target as HTMLInputElement;
+                    setEmail(targetVal.value);
+                  }}
+                  required={true}
+                />
+              </FormGroup>
+              <FormGroup className='mb-3'>
+                <Input
+                  value={password}
+                  type='password'
+                  placeholder='Password'
+                  label='Password'
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const targetVal = e.target as HTMLInputElement;
+                    setPassword(targetVal.value);
+                  }}
+                  required={true}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
 
-          <Button type='submit' className='submit_btn'>
+          <div className='indicators_buttons'>
+            <div className='left disabled' ref={leftArrowRef} title='left'>
+              <FontAwesomeIcon icon={faCircleArrowLeft} />
+            </div>
+            <div className='right' ref={rightArrowRef} title='right'>
+              <FontAwesomeIcon icon={faCircleArrowRight} />
+            </div>
+          </div>
+
+          <Button type='submit' className='submit_btn' ref={singUpButtonRef}>
             Sign Up
           </Button>
         </Form>
