2c96c8032ea42bce838c5777c713fa2b0bec4269
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-01-10 08:05:39 +0200

    add Suspense to contain the whole application

--- src/constants/APIS_ROOT.ts
+++ src/constants/APIS_ROOT.ts
@@ -1,1 +1,2 @@
-export default 'https://facex-bacekend.onrender.com/';
+// export default 'https://facex-bacekend.onrender.com/';
+export default 'http://localhost:3000/';
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
+import { StrictMode, Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.scss';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import Loading from './components/Loading/Loading.tsx';
 
 const element = document.getElementById('root');
 createRoot(element!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>
+  <Suspense fallback={<Loading />}>
+    <Provider store={store}>
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    </Provider>
+  </Suspense>
 );
-
-// ReactDOM;
