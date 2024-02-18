f6b33f9240b78e1d94452ab66dbfce29d6d8b5ad
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-02-18 11:28:07 +0200

    fix some routes names

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,37 +1,36 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import UserNetwork from '../pages/Networks/Networks';
-import Jobs from '../pages/Jobs/Jobs';
+import { useSelector } from 'react-redux';
+import State from '../types/store/AuthSliceState';
 import Home from '../pages/Home/Home';
+import Jobs from '../pages/Jobs/Jobs';
 import Messages from '../pages/Messages/Messages';
 import Notifications from '../pages/Notifications/Notifications';
-import User from '../pages/Users/Users';
-import Admin from '../pages/Admin/Admin';
 import Profile from '../pages/Profile/Profile';
+import Admin from '../pages/Admin/Admin';
 import CreateAd from '../crud/Ad/CreateAd';
+import CreateArticle from '../crud/Article/Create/CreateArticle';
 import CreateBlog from '../crud/Blog/CreateBlog';
 import CreatePayment from '../crud/Payment/CreatePayment';
 import CreateReel from '../crud/Reel/CreateReel';
-import SignIn from '../pages/Authentication/SignIn';
-import SignUp from '../pages/Authentication/SignUp';
-import CreateArticle from '../crud/Article/Create/CreateArticle';
 import UploadPost from '../forms/UploadPost/UploadPost';
-import { useSelector } from 'react-redux';
+import SignUp from '../pages/Authentication/SignUp';
+import SignIn from '../pages/Authentication/SignIn';
 
 const AppRoutes = () => {
-  const { user } = useSelector((state) => state.Auth);
+  const { user } = useSelector((state: State) => state.Auth);
   return (
     <Routes>
       <Route path='/'>
         {user ? (
           <>
             <Route index element={<Home />} />
-            <Route path='/networks' element={<UserNetwork />} />
+            {/* <Route path='/networks' element={<UserNetwork />} /> */}
             <Route path='/jobs' element={<Jobs />} />
             <Route path='/messages' element={<Messages />} />
             <Route path='/notifications' element={<Notifications />} />
             <Route path='/me/profile' element={<Profile />} />
             <Route path='/admin' element={<Admin />} />
-            <Route path='/user/:id' element={<User />} />
+            {/* <Route path='/user/:id' element={<User />} /> */}
             //** Create Section **/
             <Route path='/create/'>
               <Route path='ad' element={<CreateAd />} />
